bcc3b7e0b4590072406269557b6be7bf25e28090
unknown <doaa>
2025-05-21 16:35:08 +0300

    satr

--- src/containers/layout/Footer/index.tsx
+++ src/containers/layout/Footer/index.tsx
@@ -3,7 +3,7 @@ import { Footer as AntdFooter } from "antd/es/layout/layout";
 function Footer() {
   return (
     <AntdFooter className=" p-0 bg-transparent h-[45px] flex items-center justify-center whitespace-nowrap overflow-hidden text-ellipsis">
-       masrad ©{new Date().getFullYear()}  Powered by Star
+       Satr ©{new Date().getFullYear()}  Powered by Star
     </AntdFooter>
   );
 }
